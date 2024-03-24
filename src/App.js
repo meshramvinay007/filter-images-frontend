3346868f4417042ca22582c57f2adbcf92c586e0
Vinay <meshramvinay007@gmail.com>
2024-03-25 01:02:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,171 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import ImageContainer from "./ImageContainer/ImageContainer";
+import Navbar from "./Navbar/Navbar";
+import Sidebar from "./Sidebar/Sidebar";
+import axios from "axios";
+import { getNewCategories } from "./helper";
 
 function App() {
+  const [categories, setCategories] = useState([]);
+  const [defaultCategories, setDefaultCategories] = useState([]);
+  const [filters, setFilters] = useState([]);
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/getCategories")
+      .then((res) => {
+        const categories = res.data.categories;
+        setCategories(getNewCategories(categories));
+        setDefaultCategories(getNewCategories(categories));
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    if (filters.length) {
+      axios
+        .post(`http://localhost:4000/getFilteredImages?page=${currentPage}`, {
+          categories: filters,
+        })
+        .then((res) => {
+          setLoading(false);
+          setImages(res.data.results);
+          setTotalPages(res.data.totalPages);
+        })
+        .catch((err) => {
+          setLoading(false);
+          console.log(err);
+        });
+    } else {
+      axios
+        .get(`http://localhost:4000/getAllImages?page=${currentPage}`)
+        .then((res) => {
+          setLoading(false);
+          setImages(res.data.results);
+          setTotalPages(res.data.totalPages);
+        })
+        .catch((err) => {
+          setLoading(false);
+          console.log(err);
+        });
+    }
+  }, [currentPage, filters]);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(1);
+    setLoading(true);
+    setCurrentPage(page);
+  };
+
+  const handleChange = (e, type, superId = null) => {
+    setLoading(true)
+    const { id, checked } = e.target;
+
+    if (type === "category") {
+      const supercategoryIndex = categories.findIndex(
+        (category) => superId === category.id
+      );
+      let updatedSupercategory = categories[supercategoryIndex];
+
+      if (updatedSupercategory) {
+        let updatedSubcategories = updatedSupercategory.subCategory.map(
+          (category) => {
+            return category.id === +id
+              ? { ...category, isChecked: checked }
+              : category;
+          }
+        );
+        updatedSupercategory = {
+          ...updatedSupercategory,
+          subCategory: updatedSubcategories,
+          isChecked: checked,
+        };
+        let updatedSuperCategories = categories.map((category) =>
+          category?.id === superId ? updatedSupercategory : category
+        );
+        setCategories(updatedSuperCategories);
+
+        setFilters(
+          [].concat(
+            ...updatedSuperCategories.map((category) => {
+              return category.subCategory
+                .map((subcategory) =>
+                  subcategory.isChecked ? subcategory.name : ""
+                )
+                .filter((category) => category);
+            })
+          )
+        );
+      }
+    }
+
+    if (type === "supercategory") {
+      const supercategoryIndex = categories.findIndex(
+        (category) => id === category.id
+      );
+      let updatedSupercategory = categories[supercategoryIndex];
+
+      if (updatedSupercategory) {
+        let updatedSubcategories = updatedSupercategory.subCategory.map(
+          (category) => ({
+            ...category,
+            isChecked: checked,
+          })
+        );
+        updatedSupercategory = {
+          ...updatedSupercategory,
+          subCategory: updatedSubcategories,
+          isChecked: checked,
+        };
+        let updatedSuperCategories = categories.map((category) =>
+          category?.id === id ? updatedSupercategory : category
+        );
+        setCategories(updatedSuperCategories);
+
+        setFilters(
+          [].concat(
+            ...updatedSuperCategories.map((category) => {
+              return category.subCategory
+                .map((subcategory) =>
+                  subcategory.isChecked ? subcategory.name : ""
+                )
+                .filter((category) => category);
+            })
+          )
+        );
+      }
+    }
+
+    //filtering out all filters
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Sidebar
+        categories={categories}
+        setCategories={setCategories}
+        defaultCategories={defaultCategories}
+        setDefaultCategories={setDefaultCategories}
+        filters={filters}
+        handleChange={handleChange}
+        setFilters={setFilters}
+      />
+      <ImageContainer
+        images={images}
+        setImages={setImages}
+        totalPages={totalPages}
+        setTotalPages={setTotalPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        loading={loading}
+        setLoading={setLoading}
+        handlePageChange={handlePageChange}
+      />
     </div>
   );
 }
